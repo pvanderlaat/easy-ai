7b57564299c88630470344ca1ed41f006d958e81
pvanderlaat <pvanderlaat@gmail.com>
2023-03-22 12:38:35 -0400

    Limit dnd functionality

--- src/screens/MadLib.js
+++ src/screens/MadLib.js
@@ -10,24 +10,29 @@ import {DndContext, MultipleContainers} from '@dnd-kit/core';
 
 
 export default function MadLib() {
-    const containers = ['A', 'B', 'C'];
+    const [filled, setFilled] = useState({
+        'Noun': null,
+        'Adjective': null,
+        'Verb': null
+    })
+    const containers = ['Noun', 'Adjective', 'Verb'];
     const [draggables, setdraggables] = useState({
-        '0': null,
-        '1': null,
-        '2': null,
-        '3': null,
-        '4': null,
-        '5': null,
-        '6': null,
-        '7': null,
-        '8': null,
-        '9': null,
-        '10': null,
-        '11': null,
-        '12': null,
-        '13': null,
-        '14': null,
-        '15': null,
+        'car': [null, 'Noun'],
+        'tree': [null, 'Noun'],
+        'bear': [null, 'Noun'],
+        'dog': [null, 'Noun'],
+        'cat': [null, 'Noun'],
+        'red': [null, 'Adjective'],
+        'sad': [null, 'Adjective'],
+        'blue': [null, 'Adjective'],
+        'tall': [null, 'Adjective'],
+        'shiny': [null, 'Adjective'],
+        'wet': [null, 'Adjective'],
+        'dancing': [null, 'Verb'],
+        'eating food': [null, 'Verb'],
+        'punching the air': [null, 'Verb'],
+        'pointing upwards': [null, 'Verb'],
+        'stretching': [null, 'Verb'],
     });
     const [draggableList, setDraggableList] = useState([]);
     useEffect(() => {
@@ -42,27 +47,60 @@ export default function MadLib() {
     function render() {
         return (
             <DndContext onDragEnd={handleDragEnd}>
-                {draggables['0']}
-                {draggables['1']}
-                <br></br>
-                ----
-                {containers.map((id) => (
-                    // We updated the Droppable component so it would accept an `id`
-                    // prop and pass it to `useDroppable`
-                    <Droppable key={id} id={id}>
-                    Drop here {id}
-                    {draggableList.map((draggable) => {
-                        return draggables[draggable.props.id] === id ? draggable : null
-                    })}
-                    </Droppable>
-                ))}
-
-                {
-                    
-                }
-                {draggableList.map((draggable) => {
-                    return draggables[draggable.props.id] === null ? draggable : null
-                })} 
+                <div
+                    style={{
+                        width: '90%',
+                        height: '30vh',
+                        backgroundColor: 'gray',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        alignItems: 'center'
+                    }}
+                >
+                    {containers.map((id) => (
+                            // We updated the Droppable component so it would accept an `id`
+                            // prop and pass it to `useDroppable`
+                            <Droppable key={id} id={id}>
+                            <h1 style={{display: 'flex'}}>{id}</h1>
+                            {draggableList.map((draggable) => {
+                                return draggables[draggable.props.id][0] === id ? draggable : null
+                            })}
+                            </Droppable>
+                    ))}
+                </div>
+                <br></br><br></br><br></br>
+                <div
+                    style={{
+                        width: '90%',
+                        height: '30vh',
+                        backgroundColor: 'gray',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        alignItems: 'center'
+                    }}
+                >
+                    {/* Render the nouns */}
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        Nouns
+                        {draggableList.map((draggable) => {
+                            return draggables[draggable.props.id][0] === null && draggables[draggable.props.id][1] === 'Noun' ? draggable : null
+                        })} 
+                    </div>
+                    {/* Render the adjectives */}
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        Adjectives
+                        {draggableList.map((draggable) => {
+                            return draggables[draggable.props.id][0] === null && draggables[draggable.props.id][1] === 'Adjective' ? draggable : null
+                        })} 
+                    </div>
+                    {/* Render the verbs */}
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        Verbs
+                        {draggableList.map((draggable) => {
+                            return draggables[draggable.props.id][0] === null && draggables[draggable.props.id][1] === 'Verb' ? draggable : null
+                        })} 
+                    </div>
+                </div>
 
                 
             </DndContext>
@@ -71,12 +109,33 @@ export default function MadLib() {
 
     function handleDragEnd(event) {
         const {over} = event;
+        let newDraggables = { ...draggables}
         if (over) {
-            let newDraggables = { ...draggables}
-            newDraggables[event.active.id] = over.id 
-            setdraggables(newDraggables);
-            console.log(draggables)
+            if (newDraggables[event.active.id][1] != over.id) {
+                alert(event.active.id + ' is not a(n) ' + over.id)
+                newDraggables[event.active.id][0] = null
+            }
+            else if (filled[over.id] == null) {
+                newDraggables[event.active.id][0] = over.id                 
+                let newFilled = { ...filled}
+                newFilled[over.id] = event.active.id
+                setFilled(newFilled)
+            }
+            else {
+                console.log("A")
+                console.log(filled)
+                alert("The " + over.id + " box already contains a word")
+            }
         }
+        else {
+            if (filled[newDraggables[event.active.id][1]] === event.active.id) {
+                let newFilled = { ...filled}
+                newFilled[newDraggables[event.active.id][0]] = null
+                newDraggables[event.active.id][0] = null
+                setFilled(newFilled)
+            }
+        }
+        setdraggables(newDraggables);
     }
 
     return render();
